1cd513a7f66ba94935472e7193c6a0bf6e49ebc4
Kyle E. Mitchell <kyle@kemitchell.com>
2018-03-15 01:29:57 -0700

    Add plugin to log selection

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var EditorState = require('prosemirror-state').EditorState
 var EditorView = require('prosemirror-view').EditorView
+var Plugin = require('prosemirror-state').Plugin
 var Schema = require('prosemirror-model').Schema
-var schema = require('prosemirror-schema-basic').schema
 var exampleSetup = require('prosemirror-example-setup').exampleSetup
+var schema = require('prosemirror-schema-basic').schema
 
 window.view = new EditorView(
   document.getElementById('editor'),
@@ -13,7 +14,17 @@ window.view = new EditorView(
           schema.text('test')
         ])
       ]),
-      plugins: exampleSetup({schema})
+      plugins: exampleSetup({schema}).concat(
+        new Plugin({
+          props: {
+            decorations: function (state) {
+              console.log(
+                state.selection.empty ? 'empty' : 'selected'
+              )
+            }
+          }
+        })
+      )
     })
   }
 )
